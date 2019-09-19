f813712fa5809b3411d1c76086982a46a2bbdae8
kenigbolo <expensivestephen@hotmail.com>
2019-09-19 14:44:23 +0300

    Updates

---
+++ precache-manifest.a3ab2adcfc187c69ebff33b1c9d87f76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391a809dc9efa8547752e336cbfa69a3",
+    "url": "/code-afrique-react-lesson-1/index.html"
+  },
+  {
+    "revision": "eaff44b0655daae09acb",
+    "url": "/code-afrique-react-lesson-1/static/css/main.22f73310.chunk.css"
+  },
+  {
+    "revision": "8e5693051da17df7035c",
+    "url": "/code-afrique-react-lesson-1/static/js/2.2a048a5b.chunk.js"
+  },
+  {
+    "revision": "eaff44b0655daae09acb",
+    "url": "/code-afrique-react-lesson-1/static/js/main.085b6301.chunk.js"
+  },
+  {
+    "revision": "70750d11ca1633fb701e",
+    "url": "/code-afrique-react-lesson-1/static/js/runtime~main.1a62b895.js"
+  }
+]);
